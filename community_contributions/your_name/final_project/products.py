c2659b948a3d5d7ccf7173293669d10a37ec0f6a
shayan-sl <shynsalimian@gmail.com>
2025-12-26 23:04:19 +0330

    Add files via upload

---
+++ community_contributions/your_name/final_project/products.py
@@ -0,0 +1,26 @@
+products = [
+    {
+        "id": 1,
+        "name": "Samsung Galaxy S25",
+        "price": 92000000,
+        "description": "پرچمدار سامسونگ با دوربین قوی و پردازنده سریع"
+    },
+    {
+        "id": 2,
+        "name": "Apple iPhone 17",
+        "price": 165000000,
+        "description": "گوشی قدرتمند اپل با کیفیت ساخت بالا"
+    },
+    {
+        "id": 3,
+        "name": "Xiaomi Redmi Note 13",
+        "price": 32000000,
+        "description": "اقتصادی و مناسب استفاده روزمره"
+    },
+    {
+        "id": 4,
+        "name": "Samsung Galaxy A34",
+        "price": 48000000,
+        "description": "میان‌رده با باتری قوی و نمایشگر خوب"
+    }
+]
